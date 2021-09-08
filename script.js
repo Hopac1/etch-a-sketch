8804f5513608328280f62b525ab3584bf0c3b815
Hopac1 <chris1101@hotmail.co.uk>
2021-09-08 13:33:13 +0100

    add click event listener, need to fix

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
+
 const container = document.getElementById("container");
+const gridSizeSlider = document.getElementById("grid-size");
+// const gridSizeValue = document.getElementById("grid-size").value;
 
-
-function makeGrid(size) {
-    for (j = 0; j < size; j++) {
-        for (i = 0; i < size; i++) {
+function makeGrid() {
+    // clearCurrentGrid()
+    for (j = 0; j < document.getElementById("grid-size").value; j++) {
+        for (i = 0; i < document.getElementById("grid-size").value; i++) {
             let div = document.createElement("div");
             div.style.height = "20px";
             div.style.width = "20px";
             div.style.background = "red";
             div.style.display = "grid";
-            div.style.gridTemplateColumns = "repeat(size, 20px";
-            div.style.gridTemplateRows = "repeat(size, 20px";
+            // div.style.gridTemplateColumns = "repeat(size, 20px";
+            // div.style.gridTemplateRows = "repeat(size, 20px";
 
-            document.getElementById("container").style.gridTemplateColumns = `repeat(${size}, 20px)`;
-            document.getElementById("container").style.gridTemplateRows = `repeat(${size}, 20px)`
-
+            document.getElementById("container").style.gridTemplateColumns = `repeat(${document.getElementById("grid-size").value}, 20px)`;
+            document.getElementById("container").style.gridTemplateRows = `repeat(${document.getElementById("grid-size").value}, 20px)`;
             document.getElementById("container").appendChild(div);
         }
     } 
 }
 
-let gridSize = document.getElementById("grid-size").value
-console.log(gridSize);
+// Event Listeners
+document.getElementById("grid-size").addEventListener("click", makeGrid);
+
 // If the number the user types isn't typeof === number then show error pop up if possible.
