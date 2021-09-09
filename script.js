538973bdad7d6e2d06f60fe04ca73c8a14d50089
Hopac1 <chris1101@hotmail.co.uk>
2021-09-09 17:20:54 +0100

    create show grid size value and clear current grid functions

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-
 const container = document.getElementById("container");
-const gridSizeSlider = document.getElementById("grid-size");
-// const gridSizeValue = document.getElementById("grid-size").value;
+const gridSizeSlider = document.getElementById("range");
+const gridSizeLabel = document.getElementById("range-label");
 
 function makeGrid() {
-    // clearCurrentGrid()
-    for (j = 0; j < document.getElementById("grid-size").value; j++) {
-        for (i = 0; i < document.getElementById("grid-size").value; i++) {
+    clearCurrentGrid()
+    showGridSizeValue()
+    for (j = 0; j < gridSizeSlider.value; j++) {
+        for (i = 0; i < gridSizeSlider.value; i++) {
             let div = document.createElement("div");
-            div.style.height = "20px";
-            div.style.width = "20px";
+            // div.style.height = "30px";
+            // div.style.width = "30px";
             div.style.background = "red";
             div.style.display = "grid";
             // div.style.gridTemplateColumns = "repeat(size, 20px";
             // div.style.gridTemplateRows = "repeat(size, 20px";
 
-            document.getElementById("container").style.gridTemplateColumns = `repeat(${document.getElementById("grid-size").value}, 20px)`;
-            document.getElementById("container").style.gridTemplateRows = `repeat(${document.getElementById("grid-size").value}, 20px)`;
+            document.getElementById("container").style.gridTemplateColumns = `repeat(${gridSizeSlider.value}, 10px)`;  // Swap px and .value 
+            document.getElementById("container").style.gridTemplateRows = `repeat(${gridSizeSlider.value}, 10px)`;
             document.getElementById("container").appendChild(div);
         }
     } 
 }
 
+// Remove all div squares to minimize lag when resizing the grid
+function clearCurrentGrid() {
+    while (container.lastChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function showGridSizeValue() {
+    gridSizeLabel.textContent = `${gridSizeSlider.value}`;
+}
+
 // Event Listeners
-document.getElementById("grid-size").addEventListener("click", makeGrid);
+gridSizeSlider.addEventListener("input", makeGrid);
+makeGrid();
 
 // If the number the user types isn't typeof === number then show error pop up if possible.
